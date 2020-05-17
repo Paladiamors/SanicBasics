4ea9a4acc2633bcf5cc1cf1059976125637d55d4
Justin Ho <paladiamors@yahoo.com>
2020-05-18 01:28:26 +0900

    Form updates and testing

--- utils/forms.py
+++ utils/forms.py
@@ -28,13 +28,67 @@ class Form:
     def __init__(self, request):
 
         self.request = request
+        self.fieldMap = {field["key"]: field for field in self.fields}
+        self._errors = []
 
     def parse(self):
         """
-        parses the form
+        parses the form and returns the form if the fields are valid
+        if the form is not valid, then returns an empty dictionary
         """
 
-        pass
+        data = {k: v[0] for k, v in self.request.form.items()}
+        data = self._parseFields(data)
+        data = self._missingFields(data)
+        data = self._setDefault(data)
+
+        return data if not self._errors else {}
+
+    def errors(self):
+        """
+        returns the errors
+        """
+        return self._errors
+    def _parseFields(self, data):
+        """
+        data is a dictionary
+        """
+
+        # TODO: boolean do not send back data if the field unchecked
+        for k, v in data.items():
+            if isinstance(v, self.fieldMap[k]["type"]):
+                # do nothing
+                pass
+            else:
+                try:
+                    v = self.fieldMap[k]["type"](v)
+                except ValueError:
+                    self._errors.append({k: f"Error parsing {v} expected {str(self.fieldMap[k]['type'])}"})
+
+        return data
+
+    def _missingFields(self, data):
+
+        requiredFields = {field["key"] for field in self.fields if field.get("required")}
+        formFields = set(data.keys())
+        missingFields = requiredFields.difference(formFields)
+
+        if missingFields:
+            for field in missingFields:
+                self._errors.append({field: "field is missing"})
+
+        return data
+
+    def _setDefault(self, data):
+
+        defaultFields = {field["key"] for field in self.fields if field.get("default") is not None}
+        formFields = set(data.keys())
+        missingFields = defaultFields.difference(formFields)
+        for field in missingFields:
+            value = self.fieldMap[field]["default"]
+            data[field] = value if not callable(value) else value
+
+        return data
 
     def spec(self):
         """
